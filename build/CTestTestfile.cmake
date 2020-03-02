99aa5b99d07535ba9359125a364694269de7a329
Ji Han <ji.han@tusimple.ai>
2020-03-02 11:59:18 -0700

    update

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ji.han/work_space/dbc_converter
-# Build directory: /home/ji.han/work_space/dbc_converter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "dbc_converter"
-PROJECT_SPACE_DIR = "/home/ji.han/work_space/dbc_converter/.release"
-PROJECT_VERSION = "1.0.0"
